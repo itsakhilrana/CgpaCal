3eee50a7650c45b0ec2bee6facebad00937f8193
unknown <akhilice13@gmail.com>
2021-09-04 03:04:39 +0530

    Created a simple state management component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
-import React from 'react'
+import React, { useState, createContext } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 
+export const getSemDetails = createContext()
+
 function App() {
+  let initialState = {
+    firstSem: {
+      cgpa: 0.0,
+      credit: 0.0,
+    },
+    secondSem: {
+      cgpa: 0.0,
+      credit: 0.0,
+    },
+    thirdSem: {
+      cgpa: 0.0,
+      credit: 0.0,
+    },
+    fourthSem: {
+      cgpa: 0.0,
+      credit: 0.0,
+    },
+    fivethSem: {
+      cgpa: 0.0,
+      credit: 0.0,
+    },
+    sixthSem: {
+      cgpa: 0.0,
+      credit: 0.0,
+    },
+    seventhSem: {
+      cgpa: 0.0,
+      credit: 0.0,
+    },
+    eightthSem: {
+      cgpa: 0.0,
+      credit: 0.0,
+    },
+  }
+
+  const [state, setstate] = useState(initialState)
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          
-          {/* <Route  component={PagenotFound} /> */}
-        </Switch>
-      </Router>
+      <getSemDetails.Provider value={[state, setstate]}>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+
+            {/* <Route  component={PagenotFound} /> */}
+          </Switch>
+        </Router>
+      </getSemDetails.Provider>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,133 +1,332 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { getSemDetails } from '../App'
 
 const Home = () => {
-    return (
-        <div className="  pt-20 w-full h-full pb-20  lg:pt-0 lg:h-screen  px-8 ">
-            
-            <div className=" ">
-            <div className=" lg:flex  md:mx-auto lg:space-x-20  sm:w-2/3 sm:mx-auto justify-around  lg:mx-auto  items-center lg:h-screen">
-                <div >
-                    <div className="Yr">
-                        <p>1st Semester</p>
-                        <div className="flex space-x-4 ">
-                        <div >
-                        <label>Cgpa</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                        <div>
-                        <label>Credit</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                    </div>
-                    </div>
+  
+  
+  const [state, setstate] = useContext(getSemDetails)
 
-                    <div className="Yr">
-                        <p>2nd Semester</p>
-                        <div className="flex space-x-4">
-                        <div>
-                        <label>Cgpa</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                        <div>
-                        <label>Credit</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                    </div>
-                    </div>
+   console.log(state)
 
-                    <div className="Yr">
-                        <p>3rd Semester</p>
-                        <div className="flex space-x-4">
-                        <div>
-                        <label>Cgpa</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                        <div>
-                        <label>Credit</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                    </div>
-                    </div>
-
-                    <div className="Yr">
-                        <p>4th Semester</p>
-                        <div className="flex space-x-4">
-                        <div>
-                        <label>Cgpa</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                        <div>
-                        <label>Credit</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                    </div>
-                    </div>
+  
+  
+  
+  return (
+    <div className="  pt-20 w-full h-full pb-20  lg:pt-0 lg:h-screen  px-8 ">
+      <div className=" ">
+        <div className=" lg:flex  md:mx-auto lg:space-x-20  sm:w-2/3 sm:mx-auto justify-around  lg:mx-auto  items-center lg:h-screen">
+          <div>
+            <div className="Yr">
+              <p>1st Semester</p>
+              <div className="flex space-x-4 ">
+                <div>
+                  <label>Cgpa</label>
+                  <input
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    value={state.firstSem.cgpa}
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        firstSem: { ...state.firstSem, cgpa: e.target.value },
+                      })
+                    }}
+                  ></input>
                 </div>
                 <div>
-                <div className="Yr">
-                        <p>5th Semester</p>
-                        <div className="flex space-x-4">
-                        <div>
-                        <label>Cgpa</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                        <div>
-                        <label>Credit</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                    </div>
-                    </div>
-
-                    <div className="Yr">
-                        <p>6th Semester</p>
-                        <div className="flex space-x-4">
-                        <div>
-                        <label>Cgpa</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                        <div>
-                        <label>Credit</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                    </div>
-                    </div>
-
-                    <div className="Yr">
-                        <p>7th Semester</p>
-                        <div className="flex space-x-4">
-                        <div>
-                        <label>Cgpa</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                        <div>
-                        <label>Credit</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                    </div>
-                    </div>
-
-                    <div className="Yr">
-                        <p>8th Semester</p>
-                        <div className="flex space-x-4">
-                        <div>
-                        <label>Cgpa</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                        <div>
-                        <label>Credit</label>
-                        <input className=" w-3/4 sm:w-full" type="text" placeholder="0.0" onChange = {()=>{}} ></input>
-                        </div>
-                    </div>
-                    </div>
+                  <label>Credit</label>
+                  <input
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    value={state.firstSem.credit}
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        firstSem: { ...state.firstSem, credit: e.target.value },
+                      })
+                    }}
+                  ></input>
                 </div>
-                </div>
-                
+              </div>
             </div>
 
-            
-            
+            <div className="Yr">
+              <p>2nd Semester</p>
+              <div className="flex space-x-4">
+                <div>
+                  <label>Cgpa</label>
+                  <input
+                    value={state.secondSem.cgpa}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        secondSem: { ...state.secondSem, cgpa: e.target.value },
+                      })
+                    }}
+                  ></input>
+                </div>
+                <div>
+                  <label>Credit</label>
+                  <input
+                    value={state.secondSem.credit}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        secondSem: {
+                          ...state.secondSem,
+                          credit: e.target.value,
+                        },
+                      })
+                    }}
+                  ></input>
+                </div>
+              </div>
+            </div>
+
+            <div className="Yr">
+              <p>3rd Semester</p>
+              <div className="flex space-x-4">
+                <div>
+                  <label>Cgpa</label>
+                  <input
+                    value={state.thirdSem.cgpa}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        thirdSem: { ...state.thirdSem, cgpa: e.target.value },
+                      })
+                    }}
+                  ></input>
+                </div>
+                <div>
+                  <label>Credit</label>
+                  <input
+                    value={state.thirdSem.credit}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        thirdSem: { ...state.thirdSem, credit: e.target.value },
+                      })
+                    }}
+                  ></input>
+                </div>
+              </div>
+            </div>
+
+            <div className="Yr">
+              <p>4th Semester</p>
+              <div className="flex space-x-4">
+                <div>
+                  <label>Cgpa</label>
+                  <input
+                    value={state.fourthSem.cgpa}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        fourthSem: { ...state.fourthSem, cgpa: e.target.value },
+                      })
+                    }}
+                  ></input>
+                </div>
+                <div>
+                  <label>Credit</label>
+                  <input
+                    value={state.fourthSem.credit}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        fourthSem: {
+                          ...state.fourthSem,
+                          credit: e.target.value,
+                        },
+                      })
+                    }}
+                  ></input>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="Yr">
+              <p>5th Semester</p>
+              <div className="flex space-x-4">
+                <div>
+                  <label>Cgpa</label>
+                  <input
+                    value={state.fivethSem.cgpa}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        fivethSem: { ...state.fivethSem, cgpa: e.target.value },
+                      })
+                    }}
+                  ></input>
+                </div>
+                <div>
+                  <label>Credit</label>
+                  <input
+                    value={state.fivethSem.credit}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        fivethSem: { ...state.fivethSem, credit: e.target.value },
+                      })
+                    }}
+                  ></input>
+                </div>
+              </div>
+            </div>
+
+            <div className="Yr">
+              <p>6th Semester</p>
+              <div className="flex space-x-4">
+                <div>
+                  <label>Cgpa</label>
+                  <input
+                    value={state.sixthSem.cgpa}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        sixthSem: { ...state.sixthSem, cgpa: e.target.value },
+                      })
+                    }}
+                  ></input>
+                </div>
+                <div>
+                  <label>Credit</label>
+                  <input
+                    value={state.sixthSem.credit}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        sixthSem: { ...state.sixthSem, credit: e.target.value },
+                      })
+                    }}
+                  ></input>
+                </div>
+              </div>
+            </div>
+
+            <div className="Yr">
+              <p>7th Semester</p>
+              <div className="flex space-x-4">
+                <div>
+                  <label>Cgpa</label>
+                  <input
+                    value={state.seventhSem.cgpa}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        seventhSem: {
+                          ...state.seventhSem,
+                          cgpa: e.target.value,
+                        },
+                      })
+                    }}
+                  ></input>
+                </div>
+                <div>
+                  <label>Credit</label>
+                  <input
+                    value={state.seventhSem.credit}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        seventhSem: {
+                          ...state.seventhSem,
+                          credit: e.target.value,
+                        },
+                      })
+                    }}
+                  ></input>
+                </div>
+              </div>
+            </div>
+
+            <div className="Yr">
+              <p>8th Semester</p>
+              <div className="flex space-x-4">
+                <div>
+                  <label>Cgpa</label>
+                  <input
+                    value={state.eightthSem.cgpa}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        eightthSem: {
+                          ...state.eightthSem,
+                          cgpa: e.target.value,
+                        },
+                      })
+                    }}
+                  ></input>
+                </div>
+                <div>
+                  <label>Credit</label>
+                  <input
+                    value={state.eightthSem.credit}
+                    className=" w-3/4 sm:w-full"
+                    type="number"
+                    placeholder = ""
+                    onChange={(e) => {
+                      setstate({
+                        ...state,
+                        eightthSem: {
+                          ...state.eightthSem,
+                          credit: e.target.value,
+                        },
+                      })
+                    }}
+                  ></input>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Home
